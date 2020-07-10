5561e9b13c079a83714922bed8da4022c3b1f95f
Shuqy <yangshuquan@gmail.com>
2020-07-10 15:30:40 +0800

    1

---
+++ Cocoa Touch Class.xctemplate/UITableViewControllerObjective-C/___FILEBASENAME___.m
@@ -0,0 +1,44 @@
+//___FILEHEADER___
+
+#import "___FILEBASENAME___.h"
+
+@interface ___FILEBASENAMEASIDENTIFIER___ ()
+
+@end
+
+@implementation ___FILEBASENAMEASIDENTIFIER___
+
+#pragma mark - initialize
+
+#pragma mark - life cycle
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+#pragma mark - system delegate
+
+#pragma mark - custom delegate
+
+#pragma mark - override method
+
+#pragma mark - private method
+
+#pragma mark - public method
+
+#pragma mark - getter
+
+#pragma mark - setter
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
